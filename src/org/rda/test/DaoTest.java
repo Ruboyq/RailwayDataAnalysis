025458952180bf09476b4b43294f56f833f4d9c1
yhl123 <15301162@bjtu.edu.cn>
2018-04-16 08:40:23 +0800

    user

--- src/org/rda/test/DaoTest.java
+++ src/org/rda/test/DaoTest.java
@@ -5,23 +5,23 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.junit.Test;
-import org.rda.mapper.UserAuthorityMapper;
 import org.rda.mapper.UserMapper;
 import org.rda.pojo.User;
-import org.rda.pojo.UserAuthority;
 
 public class DaoTest extends BaseTest{
 	@Resource
 	//private UserMapper userMapper;
-	private UserAuthorityMapper userAuthorityMapper;
+	private UserMapper userMapper;
 	
-//	@Test
-//	public void getUser(){
-//		System.out.println("ddddddd");
-//		//User user = userMapper.loginUser(333, "");
+	@Test
+	public void getUser(){
+		System.out.println("ddddddd");
+		userMapper.updateUserById(new User(12, "","","",22L, "", 0, "", "", "", ""));
+		
+//		User user = userMapper.loginUser(333, "");
 //		List<String> a = 
 //				userAuthorityMapper.searchUserAuthority(333);
 //		System.out.println("ddddddd");
 //		System.out.println(a.get(0));
-//	}
+	}
 }
